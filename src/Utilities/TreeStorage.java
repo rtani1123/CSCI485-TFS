752818959717b58b9d1425de8b676aba58b8f767
boqrat <r.boghrati@gmail.com>
2014-04-10 01:09:18 -0700

    treeStorage minor changes

--- src/Utilities/TreeStorage.java
+++ src/Utilities/TreeStorage.java
@@ -7,8 +7,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class TreeStorage {
-static public Tree getTree(){
-	String filename = "C:/Users/bquock/tree.txt";
+static public Tree getTree(String path){
+	String filename = path;
 	Tree myTree = null;
 //	List pDetails = null;
 	FileInputStream fis = null;
@@ -25,8 +25,8 @@ static public Tree getTree(){
 	}
 	return myTree;
 }
-static public void storeTree(Tree inTree){
-	String filename = "C:/Users/bquock/tree.txt";
+static public void storeTree(Tree inTree, String path){
+	String filename = path;
 	FileOutputStream fos = null;
 	ObjectOutputStream out = null;
 	try {
