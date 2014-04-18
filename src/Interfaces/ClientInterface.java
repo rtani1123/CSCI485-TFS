2cf9a7a6488cf4ab75bd258a9669817f7052968b
Julia Lazzari-Dean <lazzarid@usc.edu>
2014-04-17 21:14:27 -0700

    updated interfaces for client, master, and chunkserver

--- src/Interfaces/ClientInterface.java
+++ src/Interfaces/ClientInterface.java
@@ -3,5 +3,6 @@ package Interfaces;
 import java.rmi.*;
 
 public interface ClientInterface extends Remote {
-
+	//functions called by master
+	public void requestStatus(String requestType, String fullPath, boolean succeeded) throws RemoteException;
 }
