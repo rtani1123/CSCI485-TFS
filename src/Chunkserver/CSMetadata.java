c59921f0b92d10b2ee4e356651e53387d7f44327
boqrat <r.boghrati@gmail.com>
2014-04-19 16:16:01 -0700

    CommadLine done! ChunkServer in progress.

--- src/Chunkserver/CSMetadata.java
+++ src/Chunkserver/CSMetadata.java
@@ -1,5 +1,14 @@
 package Chunkserver;
 
-public class CSMetadata {
+import java.util.ArrayList;
 
+public class CSMetadata {
+	ArrayList<CSMetadataItem> csMetaDataArray = new ArrayList<CSMetadataItem>();
+	public CSMetadata(){
+		
+	}
+	public void addMetaData(long startTime, int type, int stage, String chunkhandle){
+		CSMetadataItem temp = new CSMetadataItem(startTime, type, stage, chunkhandle);
+		csMetaDataArray.add(temp);
+	}
 }
--- src/Utilities/CommandLine.java
+++ src/Utilities/CommandLine.java
@@ -4,14 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import Part1.Test1;
+import Part1.Test2;
+import Part1.Test3;
+import Part1.Test4;
+import Part1.Test5;
+import Part1.Test6;
+import Part1.Test7;
+
 public class CommandLine {
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		System.out.println(" Welcome to our Tiny File System!");
+		System.out.println(" If you don't know what to do, ask us! Simply type help ");
 		for (;;) {
 			// get user input
-			System.out.print("Please choose a function: ");
-
+			System.out.print(" > ");
 			String input = null;
 
 			try {
@@ -20,8 +28,78 @@ public class CommandLine {
 				e.printStackTrace(System.out);
 				System.exit(1);
 			}
-			System.out.println("you have entered: " + input);
+			if (input.equals("help")) {
+				System.out.println(" Test1 numberOfFolders ");
+				System.out.println(" Test2 path numberOfFiles ");
+				System.out.println(" Test3 path ");
+				System.out.println(" Test4 source destination ");
+				System.out.println(" Test5 source destination ");
+				System.out.println(" Test6 source destination ");
+				System.out.println(" Test7 fullPath ");
+			} else if (input.contains("Test1")) {
+				String [] actuallArgs = getArgs(input);
+				if (actuallArgs == null || actuallArgs.length!=1){
+					System.out.println(" Please enter appropriate number of arguments. ");
+				}else
+				Test1.main(actuallArgs);
+			} else if (input.equals("Test2")) {
+				String [] actuallArgs = getArgs(input);
+				if (actuallArgs == null || actuallArgs.length!=2){
+					System.out.println(" Please enter appropriate number of arguments. ");
+					
+				}else
+				Test2.main(actuallArgs);
+
+			} else if (input.equals("Test3")) {
+				String [] actuallArgs = getArgs(input);
+				if (actuallArgs == null || actuallArgs.length!=1){
+					System.out.println(" Please enter appropriate number of arguments. ");
+					
+				}else
+				Test3.main(actuallArgs);
+
+			} else if (input.equals("Test4")) {
+				String [] actuallArgs = getArgs(input);
+				if (actuallArgs == null || actuallArgs.length!=2){
+					System.out.println(" Please enter appropriate number of arguments. ");
+					
+				}else
+				Test4.main(actuallArgs);
+			} else if (input.equals("Test5")) {
+				String [] actuallArgs = getArgs(input);
+				if (actuallArgs == null || actuallArgs.length!=2){
+					System.out.println(" Please enter appropriate number of arguments. ");
+					
+				}else
+				Test5.main(actuallArgs);
+
+			} else if (input.equals("Test6")) {
+				String [] actuallArgs = getArgs(input);
+				if (actuallArgs == null || actuallArgs.length!=2){
+					System.out.println(" Please enter appropriate number of arguments. ");
+					
+				}else
+				Test6.main(actuallArgs);
+
+			} else if (input.equals("Test7")) {
+				String [] actuallArgs = getArgs(input);
+				if (actuallArgs == null || actuallArgs.length!=1){
+					System.out.println(" Please enter appropriate number of arguments. ");
+					
+				}else
+				Test7.main(actuallArgs);
+
+			}
 		}
 
+	}
+	public static String[] getArgs (String in){
+		String allArgs[] = in.split(" ");
+		String actuallArgs [] = new String[allArgs.length-1];
+		for (int i = 0; i < allArgs.length-1 ; i++){
+			actuallArgs[i] = allArgs[i+1];
+		}
+		
+		return actuallArgs;
 	}
 }
