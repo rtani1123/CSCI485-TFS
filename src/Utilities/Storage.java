eecff77dc1fa95bfea3a930113b5b357e4d0c583
boqrat <r.boghrati@gmail.com>
2014-04-21 18:36:21 -0700

    We can store operation logs

--- src/Utilities/Storage.java
+++ src/Utilities/Storage.java
@@ -8,45 +8,87 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Storage {
-static public Tree getTree(){
-	String filename = "tree.txt";
-	Tree myTree = null;
-//	List pDetails = null;
-	File f = new File (filename);
-	if(!f.exists())
-	{
-		System.out.println("No stored tree file exists.");
-		return null;
-	}
-	FileInputStream fis = null;
-	ObjectInputStream oin = null;
-	try {
-		if(f.length() > 0)
-		{
-			fis = new FileInputStream(filename);
-			oin = new ObjectInputStream(fis);
-			myTree = (Tree) oin.readObject();
-			oin.close();
+	static public Tree getTree() {
+		String filename = "tree.txt";
+		Tree myTree = null;
+		// List pDetails = null;
+		File f = new File(filename);
+		if (!f.exists()) {
+			System.out.println("No stored tree file exists.");
+			return null;
 		}
-	} catch (IOException ex) {
-		ex.printStackTrace();
-	} catch (ClassNotFoundException ex) {
-		ex.printStackTrace();
+		FileInputStream fis = null;
+		ObjectInputStream oin = null;
+		try {
+			if (f.length() > 0) {
+				fis = new FileInputStream(filename);
+				oin = new ObjectInputStream(fis);
+				myTree = (Tree) oin.readObject();
+				oin.close();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		return myTree;
 	}
-	return myTree;
-}
-static public void storeTree(Tree inTree){
-	String filename = "tree.txt";
-	FileOutputStream fos = null;
-	ObjectOutputStream out = null;
-	try {
-		fos = new FileOutputStream(filename);
-		out = new ObjectOutputStream(fos);
-		out.writeObject(inTree);
-		out.close();
-		System.out.println("Namespace Tree Persisted");
-	} catch (IOException ex) {
-		ex.printStackTrace();
+
+	static public void storeTree(Tree inTree) {
+		String filename = "tree.txt";
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		try {
+			fos = new FileOutputStream(filename);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(inTree);
+			out.close();
+			System.out.println("Namespace Tree Persisted");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
-}
+	static public OperationsLog getLog() {
+		String filename = "log.txt";
+		OperationsLog myLog = null;
+		// List pDetails = null;
+		File f = new File(filename);
+		if (!f.exists()) {
+			System.out.println("No stored log file exists.");
+			return null;
+		}
+		FileInputStream fis = null;
+		ObjectInputStream oin = null;
+		try {
+			if (f.length() > 0) {
+				fis = new FileInputStream(filename);
+				oin = new ObjectInputStream(fis);
+				myLog = (OperationsLog) oin.readObject();
+				oin.close();
+			}
+		} catch (IOException ex) {
+			System.out.println("Error in reading operation log");
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Error in reading operation log");
+			ex.printStackTrace();
+		}
+		return myLog;
+	}
+
+	static public void storeLog(OperationsLog inLog) {
+		String filename = "log.txt";
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		try {
+			fos = new FileOutputStream(filename);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(inLog);
+			out.close();
+			System.out.println("Operation log Persisted");
+		} catch (IOException ex) {
+			System.out.println("Error in storing operation log");
+			ex.printStackTrace();
+		}
+	}
 }
