181196fe8b424fd3a16368e59086b8bb07c6c0eb
Ryan Tani <ryan.tani1123@gmail.com>
2014-04-26 14:13:30 -0700

    deleting test interface

--- src/Interfaces/TestInterface.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface TestInterface extends Remote{
-	public int Add(int a, int b) throws RemoteException;
-	public void createFile() throws RemoteException;
-}
